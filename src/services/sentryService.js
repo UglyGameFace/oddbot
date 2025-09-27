d71a51acefd281e92de91c56a9af99e758bb33d0
UglyGameFace <85308991+UglyGameFace@users.noreply.github.com>
2025-09-27 06:01:54 -0400

    Update sentryService.js

--- src/services/sentryService.js
+++ src/services/sentryService.js
@@ -6,7 +6,7 @@ import env from '../config/env.js';
 if (env.SENTRY_DSN) {
   Sentry.init({
     dsn: env.SENTRY_DSN,
-    environment: env.NODE_ENV || 'development',
+    environment: env.NODE_ENV || 'production',
     integrations: [
       new Sentry.Integrations.Http({ tracing: true }),
       new Sentry.Integrations.Express(),
